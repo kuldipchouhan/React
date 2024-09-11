2a91cdf11e55f8ac3fda9cebea9e3fdcb36fcc06
kuldeep Chouhan <kuldeepchouhan@avalonsolution.com>
2024-09-11 14:38:54 +0530

    test

--- src/components/NetFlexSeries.jsx
+++ src/components/NetFlexSeries.jsx
@@ -1,19 +1,21 @@
 export const NetFlexSeries = () => {
-    const name = "test11" ;
-    const rating = "8.0" ;
-    let age ="16"
-  
-    const returnGenra  = () =>{
-      const genra = "RomCom";
-      return genra;
-    }
-    const  canWatch = () =>{
-  if (age >= 18) return  "Watch  Now";
-                  return "Not Avalable ";
-  
-    }  
-  
-    return (
+  const name = "test11";
+  const rating = "8.0";
+  let age = "16"
+
+  const returnGenra = () => {
+    const genra = "RomCom";
+    return genra;
+  }
+
+
+  const canWatch = () => {
+    if (age >= 18) return "Watch  Now";
+    return "Not Avalable ";
+  }
+
+
+  return (
     <div className="card">
       <img src="m1.jpg" alt="Movie Poster" />
       <div className="card-content">
@@ -21,10 +23,10 @@ export const NetFlexSeries = () => {
         <p className="card-rating">⭐ {rating}</p>
         <p className="card-rating"> {returnGenra()}</p>
         <button>{canWatch()}</button>
-        
-  
+
+
       </div>
     </div>
-    );
-  }
+  );
+}
 export default NetFlexSeries
